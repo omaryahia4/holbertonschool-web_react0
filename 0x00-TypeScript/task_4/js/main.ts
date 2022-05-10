84a6e2ff8940647fe05a80bfe050fe2915dcdcec
omaryahia4 <omar.yahia4444@gmail.com>
2022-05-10 15:03:17 +0100

    Namespace & Declaration merging

---
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -0,0 +1,29 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'best',
+  lastName: 'teacher',
+  experienceTeachingC: 10,
+};
+
+cpp.setTeacher(cTeacher);
+console.log('C++');
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+java.setTeacher(cTeacher);
+console.log('Java');
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+react.setTeacher(cTeacher);
+console.log('React');
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
