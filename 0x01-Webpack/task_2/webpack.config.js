69aa7a40a0951e42df0413ca15a06ce57d3ae4a3
omaryahia4 <omar.yahia4444@gmail.com>
2022-05-11 12:34:10 +0100

    Adding CSS & Images

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+};
