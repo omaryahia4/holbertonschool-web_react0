b8eed8a041523c6865a7a2eb7e4cdc6143a27944
omaryahia4 <omar.yahia4444@gmail.com>
2022-05-12 14:37:43 +0100

    fix

---
+++ 0x01-Webpack/task_3/js/dashboard_main.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+const _ = require('lodash');
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on(
+  'click',
+  _.debounce(updateCounter, 300, {
+    leading: true,
+    trailing: false,
+  })
+);
+
+let count = 0;
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('body').prepend('<p id="logo"></p>');
