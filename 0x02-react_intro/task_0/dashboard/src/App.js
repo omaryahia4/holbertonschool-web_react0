08d0cc6e3fefaeb5cf12656f2ffe4efc38031588
omaryahia4 <omar.yahia4444@gmail.com>
2022-05-16 17:02:52 +0100

    Basic application

---
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import HolbertonLogo from './HolbertonLogo.jpg';
+import './App.css'
+function App() {
+  return (
+    <>
+      <header className='App-header'>
+        <img className='App-logo' src={HolbertonLogo} alt='logo'></img>
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className='App-footer'>
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
