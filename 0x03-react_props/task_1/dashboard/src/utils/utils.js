785e1ed12d0d9fd5361dde2ebe4491ed4c474ac5
omaryahia4 <omar.yahia4444@gmail.com>
2022-05-23 14:57:39 +0100

    Split the Notifications component

---
+++ 0x03-react_props/task_1/dashboard/src/utils/utils.js
@@ -0,0 +1,11 @@
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+    if (isIndex === true) {return 'Holberton School';}
+    return 'Holberton School main dashboard';
+}
+export function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
