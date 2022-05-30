f9f85b5f49f9808b3d0151b7f428717a14731f8b
omaryahia4 <omar.yahia4444@gmail.com>
2022-05-30 12:00:30 +0100

    fix app

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -9,17 +9,20 @@ import CourseList from '../../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
     this.listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
     ];
     this.listNotifications = [
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New resume available' },
       { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
     ];
+  }
+  render() {
     if (!this.props.isLoggedIn) {
       return (
         <React.Fragment>
