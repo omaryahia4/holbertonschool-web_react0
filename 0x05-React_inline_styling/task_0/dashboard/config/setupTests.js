dee04b9dc6e0a47eeeb23ff844310a83fd7a949a
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-03 15:45:01 +0100

    Inline styling

--- 0x05-React_inline_styling/task_0/dashboard/config/setupTests.js
+++ 0x05-React_inline_styling/task_0/dashboard/config/setupTests.js
@@ -4,6 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-18';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
