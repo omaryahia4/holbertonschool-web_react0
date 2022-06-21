269b6e045e3d2096911bac6c640e23cadd8b2c35
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-21 14:08:48 +0100

    another method

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
-import { fromJS, getIn } from 'immutable';
+import { fromJS, getIn  } from 'immutable';
 
 export default function accessImmutableObject (object, array) {
   const map1 = fromJS(object);
-  return getIn(map1, array, undefined || '');
+  return getIn(map1, array, undefined)
 }
