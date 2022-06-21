a601fd6cd7ea0412a871df04a1e69dd8ab2a90ec
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-21 16:37:47 +0100

    Chained mutations

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-const map = Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,6 +9,6 @@ const map = Map({
   6: 'Lucas'
 });
 
-const map2 = map.withMutations(map => {
-    map.set(2, 'Benjamin').set(4, 'Oliver');
-})
+export const map2 = map.withMutations(map => {
+  map.set(2, 'Benjamin').set(4, 'Oliver');
+});
