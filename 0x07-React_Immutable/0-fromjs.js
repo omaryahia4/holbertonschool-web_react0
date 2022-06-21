bcc2fb5e85dbe1a031f816d144f8c66a31e6ec9f
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-21 13:00:25 +0100

    semistandard fix

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,5 +1,5 @@
 import { fromJS } from 'immutable';
 
-export default function getImmutableObject (object) {
-  return fromJS(object);
-}
+const getImmutableObject = (object) => fromJS(object); 
+
+export default getImmutableObject;
