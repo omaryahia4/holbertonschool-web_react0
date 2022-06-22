cd657ac1eb3f8b37839a25494d0c2769f4da8a4e
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-22 10:21:28 +0100

    merge deeply

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -2,7 +2,7 @@ import { Map } from 'immutable';
 
 export const mergeDeeplyElements = (page1, page2) => {
   if (page1 === page2) {
-    return Map.mergeDeep(page2);
+    return Map.mergeDeep(Map(page2));
   }
-  return Map(page1).mergeDeep(page2);
+  return Map(page1).mergeDeep(Map(page2));
 };
