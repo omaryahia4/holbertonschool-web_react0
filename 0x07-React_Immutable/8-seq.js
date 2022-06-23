932e6a1961a0a4e277bdb2155998b006d18a8b96
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-23 10:30:26 +0100

    fix semistandard

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,6 +1,7 @@
 import { Seq } from 'immutable';
 
-const printBestStudents = (grades) =>
-  console.log(Seq(grades).filter(x => x.score > 70).map(x => { return { ...x, firstName: x.firstName.charAt(0).toUpperCase() + x.firstName.substring(1), lastName: x.lastName.charAt(0).toUpperCase() + x.lastName.substring(1) }; }).toObject());
+function printBestStudents(grades) {
+  return console.log(Seq(grades).filter(x => x.score > 70).map(x => { return { ...x, firstName: x.firstName.charAt(0).toUpperCase() + x.firstName.substring(1), lastName: x.lastName.charAt(0).toUpperCase() + x.lastName.substring(1) }; }).toObject());
+}
 
 export default printBestStudents;
