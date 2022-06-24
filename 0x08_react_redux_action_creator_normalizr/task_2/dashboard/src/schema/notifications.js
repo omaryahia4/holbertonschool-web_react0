7ff35576035813a1f049242040f285e186bb3c27
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-24 14:34:38 +0100

    Filter a normalized Schema

--- 0x08_react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -1,4 +1,4 @@
-import * as notificationsList from '../../../notifications.json' assert {type: 'json'};
+import * as notificationsList from '../../../../notifications.json' assert {type: 'json'};
 import { normalize, schema } from "normalizr";
 
 const user = new schema.Entity("users");
