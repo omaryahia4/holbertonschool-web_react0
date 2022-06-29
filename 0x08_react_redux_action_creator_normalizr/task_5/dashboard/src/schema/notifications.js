1c71f025136b49fdc01053a5f84a9a80a97262fc
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-29 16:50:13 +0100

    Create actions for the notification list

---
+++ 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/schema/notifications.js
@@ -0,0 +1,36 @@
+import * as notificationsData from "../../notifications.json";
+import { normalize, schema } from "normalizr";
+
+const user = new schema.Entity("users");
+
+const message = new schema.Entity(
+  "messages",
+  {},
+  {
+    idAttribute: "guid",
+  }
+);
+
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message,
+});
+
+const normalizedData = normalize(notificationsData.default, [notification]);
+
+export { normalizedData };
+
+export function getAllNotificationsByUser(userId) {
+  const notifications = normalizedData.entities.notifications;
+  const messages = normalizedData.entities.messages;
+
+  const notificationsByUser = [];
+
+  for (const property in notifications) {
+    if (notifications[property].author === userId) {
+      notificationsByUser.push(messages[notifications[property].context]);
+    }
+  }
+
+  return notificationsByUser;
+}
