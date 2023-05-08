00190e789454c43b8406e66e50ff6aacad588be0
omaryahia4 <omar.yahia4444@gmail.com>
2023-05-08 14:24:01 +0100

    task3

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, './public'),
   },
   optimization: {
     splitChunks: {
